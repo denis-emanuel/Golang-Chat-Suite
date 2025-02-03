dd301be931a04174ed184144adaee83226f64bac
Denis Pop <pop.denis1902@gmail.com>
2025-02-03 20:50:09 +0200

    add cobra and viper boilerplate setup and move go workspace to backend/

---
+++ backend/cmd/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Denis Pop
+
+*/
+package main
+
+import "github.com/denis-emanuel/Golang-Chat-Suite/cmd/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- backend/controller/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/denis-emanuel/Golang-Chat-Suite/cmd
-
-go 1.22.1
